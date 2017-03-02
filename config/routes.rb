314e71a6794b7fff999d5e304820d726408e7288
Angélica María D <angelidu@gmail.com>
2017-03-01 19:15:28 -0500

    creacion de  nueva vista new, y gracion de ruta en routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'files', to: 'files#index'
   get 'files/:name', to: 'files#show', as: 'file'
+  get 'files/new', to: 'files#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
