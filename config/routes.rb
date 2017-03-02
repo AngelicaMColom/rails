29ececd3381d18f67df45c75c2a08b5477aa0763
Angélica María D <angelidu@gmail.com>
2017-03-01 19:11:35 -0500

    mi primera app web

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'files', to: 'files#index'
+  get 'files/:name', to: 'files#show', as: 'file'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
